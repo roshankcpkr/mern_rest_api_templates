510bc18d8a1b2b02057ede9bad0cf5672fe0f9cb
Roshan KC <roshankcpkr123@gmail.com>
2023-10-27 00:22:13 +0545

    added user controller adn user schema

--- rest_template_Node_TS_MO_Zod/src/routes.ts
+++ rest_template_Node_TS_MO_Zod/src/routes.ts
@@ -1,10 +1,15 @@
+import { createUserHandler } from "controller/user.controller"
 import { Express, Request, Response } from "express"
+import validateResource from "middleware/validateResource"
+import { createUserSchema } from "schema/user.schema"
 
 function routes (app: Express)
 {
     app.get('/healthcheck', (req: Request, res: Response) => {
         return res.sendStatus(200)
     })
+
+    app.post('/api/users', validateResource(createUserSchema), createUserHandler)
 }
 
 export default routes
---
+++ rest_template_Node_TS_MO_Zod/src/service/user.service.ts
@@ -0,0 +1,13 @@
+import UserModel, { UserDocument } from 'models/user.model';
+
+export async function createUser(input: any)
+{
+    try
+    {
+        return await UserModel.create(input)
+    }
+    catch(error: any)
+    {
+        throw new Error(error)
+    }
+}
