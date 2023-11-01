830dae3e9efd8fa89dc53eafb330329ddd2115bf
Roshan KC <roshankcpkr123@gmail.com>
2023-11-01 17:43:45 +0545

    environment variables setup done

--- multitenant_template/src/main.ts
+++ multitenant_template/src/main.ts
@@ -1,5 +1,6 @@
+import { env } from "./config/env"
 import { buildServer } from "./utils/server"
-
+import { logger } from "./utils/logger"
 
 async function gracefulShutdown({app}: {
     app: Awaited<ReturnType<typeof buildServer>>
@@ -12,11 +13,12 @@ async function main()
 {
     const app = await buildServer()
     app.listen({
-        port : 3000,
+        port : env.PORT,
+        host : env.HOST
     })
 
     const signals = ['SIGINT', 'SIGTERM'] as const
-
+    logger.debug(env, "Environment variables")
     for (const signal of signals)
     {
         process.on(signal, async () => {
